3f6a74e529ca464def6f3dd4f866f37080f4d254
Cseh Zsolt <zscseh93@gmail.com>
2018-02-16 19:08:45 +0100

    Add getVector() to Direction

--- src/game/Direction.kt
+++ src/game/Direction.kt
@@ -1,8 +1,26 @@
 package game
 
 enum class Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    UP {
+        override fun getVector(): Coordinate {
+            return Coordinate(-1, 0)
+        }
+    },
+    DOWN {
+        override fun getVector(): Coordinate {
+            return Coordinate(1, 0)
+        }
+    },
+    LEFT {
+        override fun getVector(): Coordinate {
+            return Coordinate(0, -1)
+        }
+    },
+    RIGHT {
+        override fun getVector(): Coordinate {
+            return Coordinate(0, 1)
+        }
+    };
+
+    abstract fun getVector(): Coordinate
 }
--- src/game/Snake.kt
+++ src/game/Snake.kt
@@ -7,15 +7,12 @@ class Snake(head: Coordinate, length: Int, private var direction: Direction) {
     val body: Deque<Coordinate> = LinkedList()
 
     init {
-        val directionVector: Coordinate = when (direction) {
-            Direction.UP -> Coordinate(1, 0)
-            Direction.DOWN -> Coordinate(-1, 0)
-            Direction.LEFT -> Coordinate(0, 1)
-            Direction.RIGHT -> Coordinate(0, -1)
-        }
-
         (1..length)
-                .map { head + directionVector * (length - it) }
+                .map { head + direction.getVector() * (it - length) }
                 .forEach { body.addFirst(it) }
+    }
+
+    fun step() {
+
     }
 }
