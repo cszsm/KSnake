0529b50d1b48d210fb6ade4b6b2228ed6e6ba063
Cseh Zsolt <zscseh93@gmail.com>
2018-02-16 17:10:02 +0100

    Add operators to position

--- src/game/Position.kt
+++ src/game/Position.kt
@@ -1,3 +1,11 @@
 package game
 
-data class Position(val row: Int, val column: Int)
+data class Position(val row: Int, val column: Int) {
+    operator fun plus(other: Position): Position {
+        return Position(row + other.row, column + other.column)
+    }
+
+    operator fun times(multiplier: Int): Position {
+        return Position(row * multiplier, column * multiplier)
+    }
+}
